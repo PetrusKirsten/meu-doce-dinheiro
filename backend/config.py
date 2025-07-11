af6c80daec153a2ef8e95767a20e1ff57bd4d923
Petrus N Kirsten <petrus.kirsten@usp.br>
2025-07-11 12:39:32 -0300

    chore: organizar .gitignore com seções para Python e Node

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-
-VERSAO      = "0.2.1"
-NOME_APP    = "Meu Doce Dinheiro"
-DESCRICAO   = "Gerenciador pessoal de finanças simples e eficiente 💸"
-AUTOR       = "Petrus K."
-REPOSITORIO = "https://github.com/seu-usuario/meu-doce-dinheiro"  # opcional
